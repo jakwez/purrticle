f9e3ceef1fe793ef585a6bb6400252bbce4d14d8
jakwez <>
2026-04-20 12:11:15 -0700

    scene of particles

--- app/main.cpp
+++ app/main.cpp
@@ -5,28 +5,37 @@
 #include <QGraphicsView>
 #include <QMainWindow>
 
+#include "Scene.h"
+
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
 
-    // 1. Create the Scene (The "Canvas" logic)
-    QGraphicsScene* scene = new QGraphicsScene();
-    scene->setSceneRect(0, 0, 400, 400);  // Define the boundaries
+    QGraphicsScene* qscene = new QGraphicsScene();
+    qscene->setSceneRect(0, 0, 400, 400);  // Define the boundaries
 
-    // 2. Create and add items
-    // You don't need a painter here; you just define properties.
-    QGraphicsRectItem* rect = scene->addRect(50, 50, 100, 60);
+    QGraphicsRectItem* rect = qscene->addRect(50, 50, 100, 60);
     rect->setBrush(Qt::GlobalColor::cyan);
-    rect->setFlag(QGraphicsItem::ItemIsMovable);  // Magic! Now you can drag it.
+    // rect->setFlag(QGraphicsItem::ItemIsMovable);  // Magic! Now you can drag it.
 
-    QGraphicsEllipseItem* circle = scene->addEllipse(200, 150, 80, 80);
+    QGraphicsEllipseItem* circle = qscene->addEllipse(200, 150, 80, 80);
     circle->setBrush(Qt::GlobalColor::magenta);
 
-    // 3. Create the View (The "Widget")
-    QGraphicsView* view = new QGraphicsView(scene);
+    QGraphicsView* view = new QGraphicsView(qscene);
     view->setRenderHint(QPainter::Antialiasing);  // Makes things look smooth
     view->setWindowTitle("Qt Graphics View Canvas");
     view->resize(450, 450);
     view->show();
+
+    Scene scene;
+    scene.particles.push_back(Vector2(30, 20));
+    scene.particles.push_back(Vector2(70, 30));
+    scene.particles.push_back(Vector2(130, 40));
+
+    for (int i = 0; i < scene.particles.size(); ++i) {
+        const Vector2& part = scene.particles[i];
+        QGraphicsEllipseItem* circle = qscene->addEllipse(part.x, part.y, 3, 3);
+        circle->setBrush(Qt::GlobalColor::magenta);
+    }
 
     return a.exec();
 }
---
+++ core/Scene.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <vector>
+
+#include "Vector2.h"
+
+class Scene {
+   public:
+    std::vector<Vector2> particles;
+};
