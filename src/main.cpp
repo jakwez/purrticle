5e28a7a82822ac85c18557d999336cca7efa96fa
jakwez <>
2026-04-20 11:41:46 -0700

    Split into code, qt app and tests

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include "MyClass.h"
-
-int main(int argc, char* argv[]) {
-    auto myObj = new MyClass();
-    std::cout << "Hello world " << myObj->doStuff() << std::endl;
-    return 0;
-}
